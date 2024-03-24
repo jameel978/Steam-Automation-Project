be8070a5d51c75823d5b2b3271a236aeba4426a6
jameelnassar <jameelna978@gmail.com>
2024-03-24 09:41:19 +0200

    bug fixes

--- Test_Runner.py
+++ Test_Runner.py
@@ -5,7 +5,7 @@ from Utils.Utils import read_json, ErrorCapturingPlugin
 
 def run_tests(test_cases,output_folder,report_name,test_type = 'serial'):
     pytest_args = []
-    pytest_args.append("-k")
+    pytest_args.append("-v")
 
     #for file_path, test_class, test_method in test_cases:
     #    pytest_args.append(f'{file_path}::{test_class}::{test_method}')
@@ -35,17 +35,17 @@ def run_tests(test_cases,output_folder,report_name,test_type = 'serial'):
 
 
 if __name__ == "__main__":
+    API_Tests = "/Tests/Steam_API"
     cur_dir = os.path.dirname(os.path.abspath(__file__))
-    API_Tests = os.path.join(cur_dir, "./Tests/Steam_API/")
-    test_config = read_json(os.path.join(cur_dir, "./Tests/Steam_API/Config/API_Tests_Config.json"))
+    test_config = read_json(os.path.join(cur_dir, "Tests/Steam_API/Config/API_Tests_Config.json"))
     test_type = test_config["test_type"]
     run_tests(API_Tests, 'Results',"API",test_type)
 
-    #UI_Tests = "./Tests/Steam_website"
-    #cur_dir = os.path.dirname(os.path.abspath(__file__))
-    #test_config = read_json(os.path.join(cur_dir, "./Tests/Steam_website/Configs/UI_Tests_Config.json"))
-    #test_type = test_config["test_type"]
-    #run_tests(UI_Tests, 'Results',"UI",test_type)
+    UI_Tests = "/Tests/Steam_website"
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    test_config = read_json(os.path.join(cur_dir, "Tests/Steam_website/Configs/UI_Tests_Config.json"))
+    test_type = test_config["test_type"]
+    run_tests(UI_Tests, 'Results',"UI",test_type)
 
 
 
